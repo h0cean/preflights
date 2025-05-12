7f35748ffa1a4e40cbe69420b54c4f526375960d
Hos <ho3yaghi@gmail.com>
2025-05-12 09:02:42 -0400

    turtle

---
+++ nvm/steps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the following command to download and install NVM:
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+# Then, restart your terminal or run:
+
+source ~/.bashrc  # or ~/.bash_profile, ~/.zshrc depending on your shell
+
+nvm --version
